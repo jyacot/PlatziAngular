842e3971cdfe154312457616290ac38fb5dfa646
Julio <jyacot@gmail.com>
2018-07-04 15:00:11 -0400

    Feature: Insert de personas en Firebase Firestore

--- src/app/services/personas.services.ts
+++ src/app/services/personas.services.ts
@@ -1,18 +1,25 @@
 import { Injectable } from "@angular/core";
+import { AngularFirestore } from "angularfire2/firestore";
+import { Observable } from "rxjs";
+// import {sha256} from "crypto-js/sha256"
 
 @Injectable()
 
 export class PersonasServices{
-    personas:any = [];
+    personas:Observable<any[]>;
+    constructor(private db:AngularFirestore){
+        this.personas = db.collection("personas").valueChanges();
+    }
     public getPersonas(){
         return this.personas;
     };
     public addPersona(persona){
-        let personaPresente = this.personas.find(p=>persona.nombre ==p.nombre);
-        if(personaPresente){
-            this.personas[this.personas.findIndex(p=>persona.nombre ==p.nombre)] = persona;
-        }else{
-            this.personas.push(persona);
-        }
+        let ref = this.db.collection("personas")
+        console.log(ref)
+        // persona.id = Date.now().toString()
+        // this.db.collection("personas").add(persona)
     };
+    public delPersona(id){
+
+    }
 }
